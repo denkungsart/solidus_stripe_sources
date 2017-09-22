c9791e5c90198db68632359af78b02454b229732
Evgeny Sugakov <shiroginne@gmail.com>
2017-09-22 12:32:40 +0200

    Set pending message for order after user returned from SOFORT

--- app/controllers/solidus_stripe_sources/returns_controller.rb
+++ app/controllers/solidus_stripe_sources/returns_controller.rb
@@ -1,6 +1,5 @@
 module SolidusStripeSources
   class ReturnsController < Spree::StoreController
-    # update source + failed + is return success?
     def show
       @order = Spree::Order.find_by!(number: params[:id])
       source = Spree::StripeSource
@@ -9,6 +8,7 @@ module SolidusStripeSources
           user_id: @order.user_id
       )
       update_source(source)
+      set_pending_flash_notice
 
       if @order.payments.all? { |payment| payment.completed? || payment.invalid? || payment.failed? }
         redirect_to spree.order_path(@order)
@@ -18,6 +18,10 @@ module SolidusStripeSources
     private
       def update_source(payment)
         SolidusStripeSources::SourceUpdater.new(payment).fetch_and_update
+      end
+
+      def set_pending_flash_notice
+        flash.notice = Spree.t(:order_processing)
       end
   end
 end
