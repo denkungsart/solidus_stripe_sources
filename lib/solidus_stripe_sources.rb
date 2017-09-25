bb2e7c199345f146114d74aae0cbb10b2d8f26d0
Evgeny Sugakov <shiroginne@gmail.com>
2017-09-25 10:34:12 +0200

    Direct require 'stripe_connection'

--- lib/solidus_stripe_sources.rb
+++ lib/solidus_stripe_sources.rb
@@ -1,5 +1,6 @@
 require "solidus_core"
 require "solidus_support"
+require "solidus_stripe_sources/stripe_connection"
 require "solidus_stripe_sources/services/source_updater"
 require "solidus_stripe_sources/engine"
 
