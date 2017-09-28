420089c5baa643a24d21e9775ef7444dfdade0dd
Evgeny Sugakov <shiroginne@gmail.com>
2017-09-28 16:43:56 +0200

    Process payment with Webhook only if payment not in final state (otherwise error will be raised)

--- lib/solidus_stripe_sources/webhook.rb
+++ lib/solidus_stripe_sources/webhook.rb
@@ -1,13 +1,13 @@
 module SolidusStripeSources
   class Webhook
     # https://stripe.com/docs/sources/sofort
-    SUPPORTED_EVENTS = %w(
+    SUPPORTED_EVENTS = %w[
       source.chargeable
       source.failed
       source.canceled
       charge.succeeded
       charge.failed
-    ).freeze
+    ].freeze
 
     def self.build_event(body, signature, secret_key)
       Stripe::Webhook.construct_event(body, signature, secret_key)
@@ -21,8 +21,8 @@ module SolidusStripeSources
     end
 
     def event_handler
-      event_object = event.type.split(".").first
-      raise "Unsupported object" unless event_object
+      event_object = event.type.split('.').first
+      raise 'Unsupported object' unless event_object
 
       @event_handler ||= "SolidusStripeSources::Events::#{event_object.capitalize}"
         .constantize.new(event, payment_source)
@@ -31,7 +31,7 @@ module SolidusStripeSources
     def process
       # TODO: source pending - nothing, invalid/cancel - invalid payment
       # TODO: charge success - process, invalid/cancel - invalid
-      event_handler.process
+      event_handler.process unless event_handler.payment.final_state?
     end
   end
 end
--- spec/lib/solidus_stripe_sources/webhook_spec.rb
+++ spec/lib/solidus_stripe_sources/webhook_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 module SolidusStripeSources
   describe Webhook do
     let!(:source) do
       FactoryGirl.create(
         :stripe_source,
-        token: "src_1B3QlvBxRuikat2VaYeRCaJ7",
+        token: 'src_1B3QlvBxRuikat2VaYeRCaJ7',
         data: {
-          client_secret: "src_client_secret_BQHK7eWEWXE20ZciZ9VC2rUA",
+          client_secret: 'src_client_secret_BQHK7eWEWXE20ZciZ9VC2rUA',
           redirect: {}
         }
       )
@@ -16,19 +16,47 @@ module SolidusStripeSources
     let(:stripe_event) { Stripe::Event.construct_from(params) }
     subject { described_class.new(stripe_event, source) }
 
-    context "charge" do
-      let(:params) { { type: "charge.pending" } }
+    context 'charge' do
+      let(:params) { { type: 'charge.pending' } }
 
-      it "builds proper event handler" do
-        expect(subject.event_handler).to be_kind_of(SolidusStripeSources::Events::Charge)
+      it 'builds proper event handler' do
+        expect(subject.event_handler)
+          .to be_kind_of(SolidusStripeSources::Events::Charge)
       end
     end
 
-    context "source" do
-      let(:params) { { type: "source.pending" } }
+    context 'source' do
+      let(:params) { { type: 'source.pending' } }
 
-      it "builds proper event handler" do
-        expect(subject.event_handler).to be_kind_of(SolidusStripeSources::Events::Source)
+      it 'builds proper event handler' do
+        expect(subject.event_handler)
+          .to be_kind_of(SolidusStripeSources::Events::Source)
+      end
+    end
+
+    describe '#process' do
+      let(:params) { { type: 'source.pending' } }
+      let(:event_handler) { subject.event_handler }
+      before { allow(event_handler).to receive(:payment) { payment } }
+
+      context 'when payment in final state' do
+        let(:payment) { instance_double('Spree::Payment', final_state?: true) }
+
+        it 'does nothing' do
+          expect(event_handler).not_to receive(:process)
+
+          subject.process
+        end
+      end
+
+      context 'when payment not in final state' do
+        let(:payment) { instance_double('Spree::Payment', final_state?: false) }
+
+        it 'processed payment' do
+          expect(event_handler).to receive(:process)
+
+          subject.process
+        end
       end
     end
   end
