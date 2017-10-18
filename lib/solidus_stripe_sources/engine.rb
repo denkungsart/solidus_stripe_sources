05128c4dd080343721c9e7ede158b50476b24832
Evgeny Sugakov <shiroginne@gmail.com>
2017-10-18 10:10:00 +0200

    Remove sofort.png from assets. Use CDN instead

--- lib/solidus_stripe_sources/engine.rb
+++ lib/solidus_stripe_sources/engine.rb
@@ -10,11 +10,6 @@ module SolidusStripeSources
       Spree::PermittedAttributes.source_attributes.concat [:source, :return_url]
     end
 
-    initializer "solidus_stripe_sources.assets.precompile" do |app|
-      Rails.application.config.assets.paths << root.join("app", "assets", "images", "solidus_stripe_sources")
-      Rails.application.config.assets.precompile += %w( solidus_stripe_sources/sofort.png )
-    end
-
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
