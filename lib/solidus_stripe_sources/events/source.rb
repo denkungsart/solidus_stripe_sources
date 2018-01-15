d024dc056a9bc230a750a75507d95438b59bd57e
Evgeny Sugakov <shiroginne@gmail.com>
2018-01-15 18:08:19 +0100

    Revert "Check payment if it can be invalidated before invalidation"
    This reverts commit 8782c075f308101424f770aa8584be967eb9e65b.

--- lib/solidus_stripe_sources/events/source.rb
+++ lib/solidus_stripe_sources/events/source.rb
@@ -8,8 +8,7 @@ module SolidusStripeSources
         when "source.failed"
           payment.failure!
         when "source.canceled"
-          # NOTE: temporary solution
-          payment.invalidate! if payment.can_invalidate?
+          payment.invalidate!
         when "source.pending"
           payment.pend!
         end
